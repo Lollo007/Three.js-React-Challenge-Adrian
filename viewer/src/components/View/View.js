bb3abd721da93b55ec50595489ce2dd77a3ff060
Adrian <uihsc@student.kit.edu>
2020-10-23 18:04:42 +0200

    Code refactoring

--- viewer/src/components/View/View.js
+++ viewer/src/components/View/View.js
@@ -15,26 +15,26 @@ class View extends Component {
   }
 
   componentDidMount() {
+    const mesh_representation = file['07Enbsqm9C7AQC9iyBwfSD'].mesh_representation;
+    const geometry = new THREE.Geometry();
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     let renderer = new THREE.WebGLRenderer();
     let raycaster = new THREE.Raycaster();
     let mouse = new THREE.Vector2();
+    const material = new THREE.MeshStandardMaterial({color: this.meshColor});
+
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     camera.position.z = 25;
 
-    {
-      const color = 0xFFFFFF;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(0, 0, 40);
-      scene.add(light);
-    }
+    // Lighting
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(0, 0, 40);
+    scene.add(light);
 
-    const mesh_representation = file['07Enbsqm9C7AQC9iyBwfSD'].mesh_representation;
-
-    const geometry = new THREE.Geometry();
 
     for (let i = 0; i < mesh_representation.positions.length; i=i+3) {
       geometry.vertices.push(
@@ -52,59 +52,41 @@ class View extends Component {
           mesh_representation.normals[i+2]
       );
 
-
       let face = new THREE.Face3(
           mesh_representation.indices[i],
           mesh_representation.indices[i+1],
           mesh_representation.indices[i+2],
           normal
       );
-
       geometry.faces.push(face);
     }
 
+    // Normals not displayed correct -> Recalculate
     geometry.computeFaceNormals();
 
-    //const color = new THREE.Color( 0xffaa00 )
-    const material = new THREE.MeshStandardMaterial({color: this.meshColor});
-
     let instance = new THREE.Mesh(geometry, material)
-
     instance.position.set(0,0,0);
-
-    scene.add( instance );
-
-    function resizeRendererToDisplaySize(renderer) {
-      const canvas = renderer.domElement;
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
-      const needResize = canvas.width !== width || canvas.height !== height;
-      if (needResize) {
-        renderer.setSize(width, height, false);
-      }
-      return needResize;
-    }
+    instance.rotateX(0.6);
+    scene.add(instance);
 
     function render(time) {
+      //Make Object Rotate
+      /*
       time *= 0.001
-      if (resizeRendererToDisplaySize(renderer)) {
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-      }
-
       const speed = .5;
       const rot = time * speed;
-      //instance_1.rotation.x = rot;
-      instance.material = new THREE.MeshStandardMaterial({color: this.meshColor});
+      instance.rotation.y = rot;
+      */
 
+      instance.material.color = this.meshColor;
       renderer.render(scene, camera);
 
       requestAnimationFrame(render.bind(this));
     }
+
     requestAnimationFrame(render.bind(this));
 
-
+    // Toggle-Color-On-Click functionality
     renderer.domElement.addEventListener("click", onclick.bind(this), true);
 
     function onclick(event) {
